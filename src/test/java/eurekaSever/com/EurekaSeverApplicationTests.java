039e76b9784673371180aa8f421b216b3fac241f
angelovilla <angelmari435@gmail.com>
2022-02-03 23:41:30 -0500

    proyecto

---
+++ src/test/java/eurekaSever/com/EurekaSeverApplicationTests.java
@@ -0,0 +1,13 @@
+package eurekaSever.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaSeverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
